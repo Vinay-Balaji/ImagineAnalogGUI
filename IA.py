3c5e3e9901f20c02fa8a8a13be73a17079fa9477
Vinayajitesh Balaji <55262835+Vinay-Balaji@users.noreply.github.com>
2022-04-20 21:50:38 -0400

    Update IA.py
    Updated Code for Slider and Add Effect

--- IA.py
+++ IA.py
@@ -1,6 +1,15 @@
 import tkinter as tk
+from turtle import home
+import tkinter.font as font
 window_height = 800
 window_width = 480
+homeNames = ["distortion", "+", "+", "+", "+", "+"]
+#plusFont = None
+def sliderCommand(var, label): 
+    label['text'] = var
+def onClickGeneric(window, frameToForget, createFrame):
+    frameToForget.grid_forget()
+    createFrame(window)
 def onClick(window,homeframe):
     homeframe.grid_forget()
     createMenuFrame(window)
@@ -14,10 +23,19 @@ def createHomeFrame(window):
     homeframe.columnconfigure(2, weight=1)
     homeframe.rowconfigure(0, weight = 1)
     homeframe.rowconfigure(1, weight = 1)
-    names = ["AddEffect", "AddEffect", "AddEffect", "AddEffect", "AddEffect", "AddEffect"]
+    colors = ["red", "orange", "yellow", "green", "light blue", "purple"]
+    
     for i in range(6):
-        tk.Button(homeframe, text= names[i], bg="light blue", fg="black",
-            command=lambda: onClick(window,homeframe)).grid(row = int(i/3), column = int(i)%3, sticky=tk.NSEW)
+        temp = tk.Frame(homeframe, bg=colors[i])
+        temp.columnconfigure(0, weight=1)
+        temp.columnconfigure(1, weight=5)
+        temp.columnconfigure(2, weight=1)
+        temp.rowconfigure(0,weight=1)
+        temp.rowconfigure(1,weight=5)
+        temp.rowconfigure(2,weight=1)
+        temp.grid(row=int(i/3), column=int(i)%3, sticky=tk.NSEW, padx=25, pady=25)
+        tk.Button(temp, text= homeNames[i], bg="black", fg="white", bd = 0,  font=plusFont if homeNames[i]=="+" else wordFont,
+            command=lambda: onClick(window,homeframe)).grid(row = 1, column = 1, sticky=tk.NSEW)
     homeframe.grid(row=0, column=0, sticky=tk.NSEW)
     return homeframe
 def createMenuFrame(window):
@@ -38,22 +56,38 @@ def createMenuFrame(window):
     return menuframe
 def createDistortionFrame(window):
     distortionframe = tk.Frame(window, bg="black", width = window_width, height = window_height)
-    distortionframe.columnconfigure(0, weight=1)
+    distortionframe.columnconfigure(0, weight=3)
+    distortionframe.columnconfigure(1, weight=1)
+    distortionframe.columnconfigure(2, weight=1)
     distortionframe.rowconfigure(0, weight = 1)
     distortionframe.rowconfigure(1, weight = 1)
     distortionframe.rowconfigure(2, weight = 1)
     distortionframe.rowconfigure(3, weight = 1)
     distortionframe.rowconfigure(4, weight = 1)
-    text = tk.Label(distortionframe, text="Distortion")
+    text = tk.Label(distortionframe, text="Distortion", bg="black", fg="white", font=titleFont)
     text.grid(row = 0, column = 0, sticky=tk.NSEW)
     names = ["Level", "Tone", "Distortion"]
-    slide1 = tk.Scale(window, from_= 0, to = 100, resolution = 2, orient = tk.HORIZONTAL, length = 500, width = 100, bg = "green")
-    slide1.pack()
-    # slide1.grid(row = 0, col=0, sticky=tk.NSEW)
-    slide2 = tk.Scale(window, from_= 0, to = 100, resolution = 2, orient = tk.HORIZONTAL, length = 500, width = 100, bg = "yellow")
-    slide2.pack()
-    slide3 = tk.Scale(window, from_= 0, to = 100, resolution = 2, orient = tk.HORIZONTAL, length = 500, width = 100, bg = "light blue")
-    slide3.pack()
+    colors = ["green", "yellow", "light blue"]
+    sliderLabels = [tk.Label(distortionframe, text="0", bg="black", fg="white") for i in range(3)]
+    slider = [tk.Scale(distortionframe,  from_= 0, to = 100, resolution = 1, orient = tk.HORIZONTAL, 
+                        length = 500, width = 15, fg = colors[i], bg="black", label=n, bd=1, troughcolor=colors[i], 
+                        font=sliderFont, showvalue=0) for i, n in enumerate(names)]
+    for i, s in enumerate(slider): 
+        s.grid(row=i+1, column=0, sticky=tk.EW)
+        sliderLabels[i].grid(row=i+1, column=1, stick=tk.NSEW)
+    tk.Button(distortionframe, text= "Add Effect", bg="black", fg="white",
+            command=lambda: onClickGeneric(window,distortionframe, createHomeFrame)).grid(row = 2, column = 2, sticky=tk.EW)
+    """
+    slide1 = tk.Scale(distortionframe, from_= 0, to = 100, resolution = 1, orient = tk.HORIZONTAL, length = 500, width = 20, fg = "green", bg="black", label="test", bd=1, troughcolor="green")
+   # slide1.pack()
+    slide1.grid(row = 1, column=0, sticky=tk.EW)
+    slide2 = tk.Scale(distortionframe, from_= 0, to = 100, resolution = 1, orient = tk.HORIZONTAL, length = 500, width = 20, fg = "yellow", bg="black", relief=tk.RIDGE)
+    #slide2.pack()
+    slide2.grid(row = 2, column=0, sticky=tk.EW)
+    slide3 = tk.Scale(distortionframe, from_= 0, to = 100, resolution = 1, orient = tk.HORIZONTAL, length = 500, width = 20, fg = "light blue", bg="black", bd = 0)
+    #slide3.pack()
+    slide3.grid(row = 3, column=0, sticky=tk.EW)
+    """
     distortionframe.grid(row=0, column=0, sticky=tk.NSEW)
     return distortionframe
 
@@ -61,5 +95,10 @@ window = tk.Tk()
 window.geometry('800x480')#Pixels
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight = 1)
+plusFont = font.Font(family="Arial", size=50, weight="bold")
+wordFont = font.Font(family="Arial", size=20, weight="bold")
+sliderFont = font.Font(family="Arial", size=20) 
+titleFont = font.Font(family="Arial", size=27)
 createHomeFrame(window)
+#createDistortionFrame(window)
 window.mainloop()
